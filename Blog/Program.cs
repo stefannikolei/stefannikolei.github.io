2731aae820ad0bd598f2d91f329869e7d8a81631
Stefan Nikolei <stefan@nikolei.eu>
2020-11-04 21:57:04 +0100

    Add Initial post. add deployment to github pages

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
 
 namespace Blog
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToGitHubPagesBranch(
+                    "stefannikolei",
+                    "stefannikolei.github.io",
+                    Config.FromSetting<string>("GITHUB_TOKEN"),
+                    "main"
+                    )
+                .RunAsync();
     }
 }
